c28dce76ab1a95e024a8de2e2bfd84e4086682bc
kari4201a <kari.4201a@gmail.com>
2023-09-01 00:27:49 -0300

    Solución creada

--- eventos.js
+++ eventos.js
@@ -1,3 +1,9 @@
+document.getElementById("boton").addEventListener("click",(event)=>
+{
+    event.stopPropagation();
+    alert("Hola!");
+});
+
 document.getElementById("botonDiv").addEventListener("click", () => {
     alert("Hola, Soy el Div");
 });
